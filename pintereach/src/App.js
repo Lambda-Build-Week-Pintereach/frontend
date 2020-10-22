b308f6294bb3bc42926935846ea9f96ffae43cbd
Olivia Toolson <oliviatoolson@Olivias-MBP.lan>
2020-10-22 07:52:08 -0700

    added login/register button to nav and created routes

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import {Listarticle} from './components/ListArticle';
 import {Createarticle} from './components/CreateArticle';
 import {Editarticle} from './components/EditArticle';
 import {Detailarticle} from './components/DetailArticle';
-// import FormHome from "./components/FormHome";
+import FormHome from "./components/FormHome";
 import { GlobalProvider } from './components/reducers/GlobalContext';
 function App() {
   return (
     <GlobalProvider>
-       <header className="text-center"><h5>Pintereach</h5></header>
+      <header className="text-center">
+        <h5>Pintereach</h5>
+        <Link to="login"><button className="loginBtn">Login / Register</button></Link>
+      </header>
        <Switch>
-         {/* <Route path="" component={FormHome}/> */}
+        <Route path="/login" component={FormHome}/>
+        <Route path="/register" component={FormHome} exact/>
         <Route path="/" component={Listarticle} exact/>
         <Route path="/create" component={Createarticle} exact/>
         <Route path="/edit/:id" component={Editarticle} exact/>
